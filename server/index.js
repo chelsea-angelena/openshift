b082bcbf86880aa4d12244c83ad70b399cfb84ad
chelsea-angelena <chelsea.angelena@gmail.com>
2023-12-17 09:45:38 -0800

    first

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+express.require('dotenv').config();
+
+const port = 8080;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
