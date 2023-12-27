bf83a6afac2af92d7a5fbc7e6f8128d2c5764152
Chelsea Brown <chelsea.angelena@gmail.com>
2023-12-27 11:07:14 -0800

    set up boilerplate stuff for deploying to openshift

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I've been deployed through GH actions
         </a>
       </header>
     </div>
